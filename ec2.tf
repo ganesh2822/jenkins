1acbfaeacaaa387bca0e5b499479d2fe5adb88e4
ganesh2822 <109890661+ganesh2822@users.noreply.github.com>
2022-07-25 12:31:14 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 data "aws_ami" "latest_amazon_linux_img" {
   most_recent      = true
+  owners = ["amazon"]
   filter {
     name   = "name"
     values = ["Amazon Linux 2 Kernel 5.10 AMI 2.0.20220606.1 x86_64 HVM gp2"]
