01b733234384e66e1042ebd338a86349159a23cf
ganesh2822 <109890661+ganesh2822@users.noreply.github.com>
2022-07-25 12:44:47 +0100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,17 @@
-data "aws_ami" "latest_amazon_linux_img" {
-  most_recent      = true
-  owners = ["amazon"]
-  filter {
+data "aws_ami" "linux" {
+  most_recent  = true
+  
+   filter {
     name   = "name"
-    values = ["Amazon Linux 2 Kernel 5.10 AMI 2.0.20220606.1 x86_64 HVM gp2"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+  
+  owners = ["amazon"]
 }
 
 resource "aws_network_interface" "private_network_interface" {
