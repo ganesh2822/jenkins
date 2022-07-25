bfc12c55e9994ed256acf75f6abf1cbcca0e0656
ganesh2822 <109890661+ganesh2822@users.noreply.github.com>
2022-07-25 09:06:26 +0100

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -10,19 +10,6 @@ resource "aws_security_group" "db-sg-grp" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["${aws_network_interface.private_network_interface.id}/32"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${aws_network_interface.private_network_interface.id}/32"]
-  }
 }
 
 # CREATE SG FOR App
